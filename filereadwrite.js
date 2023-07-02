08eedcb409d2799686a0bade7033adb6371353ca
Nikhil334 <nikhil.gupta@appinventiv.com>
2023-07-02 13:57:26 +0530

    First commit

---
+++ filereadwrite.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+//fs.writeFileSync("read.txt","Welcome to the new file");
+//fs.writeFileSync("read.txt","welcome to the new file to read and write");
+
+//fs.appendFileSync("read.txt"," Please write your own thughts for nodejs");
+
+//const buf_data = fs.readFileSync("read.txt");
+
+//const buf_data = fs.readFileSync("read.txt","utf-8");
+
+//console.log(buf_data);
+
+//const file_data = buf_data.toString();
+
+//console.log(file_data);
+
+//fs.renameSync("read.txt","readwrite.txt");
+fs.renameSync("readwrite.txt","read.txt");
